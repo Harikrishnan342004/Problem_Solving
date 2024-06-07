d1f2149f062a158a07301d4fa1f2d664eb4bcadd
Hari <harikrishnan53828@gmail.com>
2024-06-07 12:36:41 +0530

    kd

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-previous_num = 0
-for i in range(1, 11):
-    #print(i)
-    sum = i + previous_num
-    print("Current Number = ", i , "previous Number =", previous_num , "sum = ", sum)
-    previous_num = i
+ 
