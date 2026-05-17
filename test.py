49fc9dfce8b2495263bd4942d8de9177441bbe2f
Harikrishnan342004 <harikrishnan53828@gmail.com>
2026-05-17 11:40:39 +0530

    hello

--- test.py
+++ test.py
@@ -6,22 +6,30 @@ def solve():
     k = int(data[1])
     a = [int(data[2 + i]) for i in range(n)]
     
-    a.sort(reverse=True)
+    NEG_INF = float('-inf')
+    # dp[j][d]: max earnings where j = items_collected % k, d = double_pending
+    dp = [[NEG_INF] * 2 for _ in range(k)]
+    dp[0][0] = 0
     
-    # Triggers at collected positions k, 2k, 3k,...
-    # Item right after trigger is doubled
-    # Max doubles in subsequence of all n items:
-    doubles = 0
-    pos = k  # first trigger position
-    while pos < n:  # there must be a next item (pos+1 <= n means pos < n)
-        doubles += 1
-        pos += k
+    for val in a:
+        new_dp = [[NEG_INF] * 2 for _ in range(k)]
+        for j in range(k):
+            for d in range(2):
+                if dp[j][d] == NEG_INF:
+                    continue
+                # Skip
+                if new_dp[j][d] < dp[j][d]:
+                    new_dp[j][d] = dp[j][d]
+                # Collect
+                earned = val * 2 if d == 1 else val
+                new_j = (j + 1) % k
+                new_d = 1 if new_j == 0 else 0
+                new_total = dp[j][d] + earned
+                if new_dp[new_j][new_d] < new_total:
+                    new_dp[new_j][new_d] = new_total
+        dp = new_dp
     
-    # Greedily double the largest 'doubles' items
-    total = sum(a)
-    for i in range(doubles):
-        total += a[i]
-    
-    print(total)
+    ans = max(dp[j][d] for j in range(k) for d in range(2) if dp[j][d] != NEG_INF)
+    print(ans)
 
 solve()
